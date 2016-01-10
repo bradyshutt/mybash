a206f61c512e30e29ab5ae93f2198bfc1c5acf56
Brady Shutt <bradyshutt@gmail.com>
2016-01-09 20:24:04 -0800

    fix

--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python 
 import subprocess
 import os
 import sys
 
+verbose = True
 
-def run_cmd(command, is_verbose=False):
-    if is_verbose: 
-        print(command)
+def run_cmd(command):
+    if verbose: 
+        print("Running command: \n " + command)
     subprocess.call([command], shell=True)
 
-def linkage(is_verbose=False):
+def linkage():
+    if verbose: 
+        print("Setting up linkage file...")
     brc = open("../.bashrc", 'a')
     brc.write("\n. ~/mybash/linkage \n")
     brc.write("\nexport TERM='xterm-256color'\n")
     brc.write("\nPS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]../\W\[\033[00m\]\$ '\n")
 
-def setup_vim(is_verbose=False):
+def setup_vim():
+    if verbose: 
+        print("Setting up vim...")
     try:
-        command = "rm -rfi ~/.vim"
-        run_cmd(command, is_verbose=is_verbose)
-
-        command = "mv ~/.vim"
-        run_cmd(command, is_verbose=is_verbose)
-
-
+        run_cmd("rm -rfI ~/.vim")
+        run_cmd("cp -r vim ~/.vim")
     except:
         print("Error, you probably need su privilages")
         exit()
@@ -34,21 +33,15 @@ def setup_vim(is_verbose=False):
 #----------------------------------------------------------------
 print("Running Setup Script...")
 args = sys.argv
-verbose = False
 
 if '-v' in args or '--verbose' in args:
-    verbose = True
+    verbose = False
 
 if '-i' in args or '--initial' in args:
     linkage()
 
-os.system("source ~/.bashrc")
-
-
-
-
-
-
+if '--vim' in args:
+    setup_vim()
 
 
 
